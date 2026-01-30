5b989c96d8cc0ae26517d207b6446a714d257ed0
Lizock <khyelyza@gmail.com>
2026-01-29 19:03:13 -0800

    Changed connection

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 
@@ -9,7 +11,8 @@ app.use(express.json());
 
 app.use("/teams", teamRoutes);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
