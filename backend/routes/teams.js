e36f1bbbd609b65718295ea03e519af48fc58b0f
Lizock <khyelyza@gmail.com>
2026-02-03 10:16:03 -0800

    Made changes to the router

--- backend/routes/teams.js
+++ backend/routes/teams.js
@@ -1,12 +1,16 @@
 import express from 'express';
 
+const express = require("express");
 const router = express.Router();
+const grouper = require("./grouper.js");
 
 router.get("/", (req, res) => {
+  const teams = grouper();
+  console.log("hit")
   res.json({
     message: "Teams endpoint working",
-    teams: []
+    teams
   });
 });
 
-export default router;
+module.exports = router;
