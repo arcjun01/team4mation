9651ad647b7e1f5956ee878ee5e9d0d7485da259
arcjun01 <arciaga.juno@student.greenriver.edu>
2026-02-12 20:18:30 -0800

    created InstructorTeamSetup component and initial styles

---
+++ frontend/src/components/InstructorTeamSetup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../css/InstructorSetup.css';
+
+const InstructorTeamSetup = () => {
+  return (
+    <div className="setup-container">
+      <h1 className="setup-title">Team Setup</h1>
+    </div>
+  );
+};
+export default InstructorTeamSetup;
