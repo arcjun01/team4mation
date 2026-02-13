758cdbdb82264a72edfcb37aa4909d00208690ae
arcjun01 <arciaga.juno@student.greenriver.edu>
2026-02-12 20:47:18 -0800

    added form fields for class size and group criteria and mounted IntructorTeamSetup in App for preview

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import InstructorTeamSetup from "./components/InstructorTeamSetup";
 import StudentSurvey from "./StudentSurvey";
 
 function App() {
@@ -6,6 +7,9 @@ function App() {
       <h1>Team4mation</h1>
       <p>Create smart student teams easily.</p>
 
+      <hr />
+      {/* Testing the InstructorTeamSetUp component */}
+      {/* <InstructorTeamSetup /> */}
       <StudentSurvey />
     </div>
   );
--- frontend/src/components/InstructorTeamSetup.jsx
+++ frontend/src/components/InstructorTeamSetup.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import '../css/InstructorSetup.css';
 
 const InstructorTeamSetup = () => {
-  return (
+    
+    const [formData, setFormData] = useState({
+    classSize: '',
+    minSize: '',
+    maxSize: '',
+    prevCourse: ''
+    });
+
+    // Will add the form's elements here later
+    return (
     <div className="setup-container">
-      <h1 className="setup-title">Team Setup</h1>
+        <h1 className="setup-title">Team Setup</h1>
     </div>
-  );
+    );
 };
 export default InstructorTeamSetup;
