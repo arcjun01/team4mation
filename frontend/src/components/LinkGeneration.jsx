d7319c8e9d760343c4be22b0866b4f5b1feab284
arcjun01 <arciaga.juno@student.greenriver.edu>
2026-02-17 13:12:12 -0800

    created a link generation component

---
+++ frontend/src/components/LinkGeneration.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/LinkGeneration.css';
+
+const LinkGeneration = () => {
+  return (
+    <div className="link-page-container">
+      <div className="card-container">
+        {/* Note to self: Work on this next */}
+        <h2 style={{ padding: '20px', textAlign: 'center' }}>Student Survey Link</h2>
+      </div>
+    </div>
+  );
+};
+export default LinkGeneration;
