43c73f2c6f084dcf0ea6adc7ad339387ed224c64
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2026-04-29 09:52:08 -0700

    removed commented code

--- backend/routes/grouper.js
+++ backend/routes/grouper.js
@@ -70,21 +70,7 @@ function enforceGroupSizes(groups, minSize, maxSize) {
 
         for (let i = 0; i < groups.length; i++) {
 
-            // If group too big → move students out
-            // while (groups[i].length > maxSize) {
-            //     let moved = false;
-
-            //     for (let j = 0; j < groups.length; j++) {
-            //         if (i !== j && groups[j].length < maxSize) {
-            //             groups[j].push(groups[i].pop());
-            //             moved = true;
-            //             changed = true;
-            //             break;
-            //         }
-            //     }
-            //     if (!moved) break;
-            // }
-
+            // If group too big -> move students out
             if (groups[i].length > maxSize) {
                 const receiver = findGroup(groups, i, g => g.length < maxSize);
                 if (receiver) {
@@ -92,16 +78,8 @@ function enforceGroupSizes(groups, minSize, maxSize) {
                     changed = true;
                 }
             }
-            //If group too small → pull from larger groups
-            // if (groups[i].length < minSize) {
-            //     for (let j = 0; j < groups.length; j++) {
-            //         if (i !== j && groups[j].length > minSize) {
-            //             groups[i].push(groups[j].pop());
-            //             changed = true;
-            //             break;
-            //         }
-            //     }
-            // }
+
+            //If group too small -> pull from larger groups
             if (groups[i].length < minSize) {
                 const donor = findGroup(groups, i, g => g.length > minSize);
                 if (donor) {
@@ -154,19 +132,9 @@ function makeBasicGroups(males, others, groupNum, targetSize) {
     }
 
     // Phase 2: Distribute remaining males
-    // while (maleIndex < males.length) {
-    //     for (let g = 0; g < groupNum && maleIndex < males.length; g++) {
-    //         groups[g].push(males[maleIndex++]);
-    //     }
-    // }
     distributeRemainder(males, maleIndex, groups);
 
     // Phase 3: Distribute remaining others
-    // while (otherIndex < others.length) {
-    //     for (let g = 0; g < groupNum && otherIndex < others.length; g++) {
-    //         groups[g].push(others[otherIndex++]);
-    //     }
-    // }
     distributeRemainder(others, otherIndex, groups);
 
     return groups;
@@ -179,130 +147,6 @@ function distributeRemainder(arr, index, groups) {
         }
     }
 }
-// Split into new file
-// function improveGroups(groups, availabilityMap) {
-//     let improvementMade = true;
-//     let passCount = 0;
-//     const MAX_PASSES = 4;
-
-//     while (improvementMade && passCount < MAX_PASSES) {
-//         improvementMade = false;
-//         passCount++;
-
-//         for (let i = 0; i < groups.length - 1; i++) {
-//             for (let j = i + 1; j < groups.length; j++) {
-//                 for (const studentA of groups[i]) {
-//                     for (const studentB of groups[j]) {
-//                         if (evalulateSwap(studentA, studentB, groups[i], groups[j], availabilityMap)) {
-//                             const indexA = groups[i].findIndex(s => s.id === studentA.id || s.student_id === studentA.student_id);
-//                             const indexB = groups[j].findIndex(s => s.id === studentB.id || s.student_id === studentB.student_id);
-
-//                             groups[i][indexA] = studentB;
-//                             groups[j][indexB] = studentA;
-//                             improvementMade = true;
-//                             break;
-//                         }
-//                     }
-//                     if (improvementMade) break;
-//                 }
-//                 if (improvementMade) break;
-//             }
-//             if (improvementMade) break;
-//         }
-//     }
-//     return groups;
-// }
-
-// function evalulateSwap(studentA, studentB, groupA, groupB, availabilityMap) {
-//     // Determine unique ID key (database uses id or student_id)
-//     const getID = (s) => s.id || s.student_id;
-
-//     const testA = groupA.map(student => getID(student) === getID(studentA) ? studentB : student);
-//     const testB = groupB.map(student => getID(student) === getID(studentB) ? studentA : student);
-
-//     if (!checkGenderRule(testA, testB)) return false;
-
-//     const currentScoreA = calculateGroupScore(groupA, availabilityMap);
-//     const currentScoreB = calculateGroupScore(groupB, availabilityMap);
-//     const testScoreA = calculateGroupScore(testA, availabilityMap);
-//     const testScoreB = calculateGroupScore(testB, availabilityMap);
-
-//     return (testScoreA + testScoreB) > (currentScoreA + currentScoreB);
-// }
-
-// function calculateScheduleOverlap(group, availabilityMap) {
-//     const availibilitySlots = {}
-//     for (const student of group) {
-//         const sid = student.id || student.student_id;
-//         const timeSlots = availabilityMap[sid] || []
-//         for (const slot of timeSlots) {
-//             availibilitySlots[slot] = (availibilitySlots[slot] || 0) + 1;
-//         }
-//     }
-
-//     let totalOverlap = 0;
-//     const minOverlap = Math.max(2, Math.ceil(group.length / 2));
-
-//     for (const slot of Object.keys(availibilitySlots)) {
-//         const count = availibilitySlots[slot];
-//         if (count >= minOverlap) {
-//             totalOverlap += count;
-//         }
-//     }
-
-//     return totalOverlap / (group.length || 1);
-// }
-
-// function calculateCommitmentSimilarity(group) {
-//     if (group.length === 0) return 0;
-//     let min = group[0].commitment, max = group[0].commitment;
-//     for (const student of group) {
-//         if (student.commitment > max) max = student.commitment;
-//         if (student.commitment < min) min = student.commitment;
-//     }
-//     let commitmentRange = max - min;
-//     return commitmentRange === 0 ? 0 : (-1 * commitmentRange);
-// }
-
-// function calculateGPASimilarity(group) {
-//     if (group.length === 0) return 0;
-//     let min = group[0].gpa, max = group[0].gpa;
-//     for (const student of group) {
-//         if (student.gpa > max) max = student.gpa;
-//         if (student.gpa < min) min = student.gpa;
-//     }
-//     let gpaSpread = max - min;
-//     return gpaSpread === 0 ? 0 : (-1 * gpaSpread);
-// }
-
-// function calculateGroupScore(group, availabilityMap) {
-//     const GPA_WEIGHT = 2, SCHEDULE_WEIGHT = 3, COMMITMENT_WEIGHT = 1;
-//     return (calculateGPASimilarity(group) * GPA_WEIGHT) +
-//         (calculateScheduleOverlap(group, availabilityMap) * SCHEDULE_WEIGHT) +
-//         (calculateCommitmentSimilarity(group) * COMMITMENT_WEIGHT);
-// }
-
-// function checkGenderRule(testA, testB) {
-//     return isGenderBalanced(testA) && isGenderBalanced(testB);
-// }
-
-// function isGenderBalanced(group) {
-//     // If group has 0 or 1 student, it's considered balanced
-//     if (group.length <= 1) return true;
-
-//     let maleCount = 0, otherCount = 0;
-//     for (const student of group) {
-//         const genderStr = student.gender ? student.gender.trim().toLowerCase() : "";
-//         if (genderStr === "male") maleCount++;
-//         else otherCount++
-//     }
-
-//     if (otherCount === 0) return true; // All males is ok
-//     if (maleCount === 0) return true;  // All others is ok
-
-//     // Allow grouping if ratio isn't extreme (max 2:1)
-//     return maleCount <= (otherCount * 2) && otherCount <= (maleCount * 2);
-// }
 
 export {
     grouper,
