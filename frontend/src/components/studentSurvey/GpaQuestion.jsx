40afd75d499a313ff3773d6fcc90bcf84f580721
Lizock <khyelyza@gmail.com>
2026-06-09 14:19:53 -0700

    Changed "GPA" to "grade" everywhere

--- frontend/src/components/studentSurvey/GpaQuestion.jsx
+++ frontend/src/components/studentSurvey/GpaQuestion.jsx
@@ -28,9 +28,9 @@ export default function GpaQuestion({ gpa, setGpa, prevCourse, error, onClear })
 
   return (
     <div className="question-container">
-      <h2 className="question-title">What was your final GPA in {courseDisplay}? Enter or adjust below</h2>
+      <h2 className="question-title">What was your final grade in {courseDisplay}? Enter or adjust below</h2>
       <p className="details">
-        You can view your GPA in{" "}
+        You can view your grades in{" "}
         <a
             href="https://csprd.ctclink.us/psc/csprd_9/EMPLOYEE/SA/c/SSR_STUDENT_ACAD_REC_FL.SSR_MD_ACAD_REC_FL.GBL?Action=U&MD=Y&GMenu=SSR_STUDENT_ACAD_REC_FL&GComp=SSR_ACADREC_NAV_FL&GPage=SCC_START_PAGE_FL&scname=CTC_ACADEMIC_RECORDS_NAVCOL&AJAXTRANSFER=Y"
             target="_blank"
@@ -46,7 +46,7 @@ export default function GpaQuestion({ gpa, setGpa, prevCourse, error, onClear })
         step="0.1"
         min="1.0"
         max="4.0"
-        value={gpa ? (Number.isInteger(gpa) ? gpa : gpa.toFixed(1)) : ""}
+        value={gpa !== "" && gpa != null ? Number(gpa).toFixed(1) : ""}
         onChange={(e) => handleGpaChange(e.target.value)}
         className="gpa-input"
       />
